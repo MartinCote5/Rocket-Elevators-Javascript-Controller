4921ef652ab3dbacd2794b5f2354373ecf3232a8
MartinCote5 <Martin.Cote2@hotmail.com>
2022-02-21 10:54:28 -0500

    project Start

---
+++ residential_controller.js
@@ -0,0 +1,33 @@
+class Column {
+    constructor(_id, _amountOfFloors, _amountOfElevators) {
+
+    };
+
+}
+
+class Elevator {
+    constructor(_id, _amountOfFloors) {
+
+    }
+
+}
+
+class CallButton {
+    constructor(_id, _floor, _direction) {
+
+    }
+}
+
+class FloorRequestButton {
+    constructor(_id, _floor) {
+
+    }
+}
+
+class Door {
+    constructor(_id) {
+
+    }
+}
+
+module.exports = { Column, Elevator, CallButton, FloorRequestButton, Door }
