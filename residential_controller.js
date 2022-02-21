50be430058117930d276e7d180ba245aa8379869
MartinCote5 <Martin.Cote2@hotmail.com>
2022-02-21 14:07:03 -0500

    attributs done

--- residential_controller.js
+++ residential_controller.js
@@ -1,12 +1,33 @@
 class Column {
     constructor(_id, _amountOfFloors, _amountOfElevators) {
-
+        this.ID = _id;
+        this.status = "online"
+        this.elevatorList = [];
+        this.callButtonList = [];
     };
+
+    requestElevator (requestedFloor, direction) {
+        
+    }
 
 }
 
 class Elevator {
     constructor(_id, _amountOfFloors) {
+        this.ID = _id
+        this.status = "idle"
+        this.direction
+        this.currentFloor
+        this.door = new Door
+        this.floorRequestButtonList = []
+        this.floorRequestList = []
+    }
+    requestFloor(requestedFloor) {
+
+    }
+    
+    
+    move() {
 
     }
 
@@ -14,19 +35,26 @@ class Elevator {
 
 class CallButton {
     constructor(_id, _floor, _direction) {
+        this.ID = _id
+        this.status = null
+        this.floor = _floor
+        this.direction = _direction
 
     }
 }
 
 class FloorRequestButton {
     constructor(_id, _floor) {
-
+        this.ID = _id
+        this.status = null
+        this.floor = _floor
     }
 }
 
 class Door {
     constructor(_id) {
-
+        this.ID = _id
+        this.status = null
     }
 }
 
