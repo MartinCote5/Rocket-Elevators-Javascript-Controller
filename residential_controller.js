abda1d881462b599307dbe09831cdec42602853e
MartinCote5 <Martin.Cote2@hotmail.com>
2022-02-22 21:16:02 -0500

    find best elevator working

--- residential_controller.js
+++ residential_controller.js
@@ -57,17 +57,65 @@ class Column {
     findElevator(floor, direction) {
         console.log("parameter value",floor, direction)
         let bestElevator 
-        let bestScore = 5
-        let referenceGap = 10000000
-        let bestElevatorInformations
+        var bestScore = 5
+        var referenceGap = 10000000
+        var bestElevatorInformations
         this.elevatorList.forEach((elevator) => {
-            if floor == elevator.currentFloor && elevator.status == "stopped" && 
 
-            console.log("in my for each",elevator.currentFloor)
-           
-        });
+            if (floor == elevator.currentFloor && elevator.status == "stopped" && direction == elevator.direction) {
+                console.log(bestScore,'GAPP')
+                
+                   bestElevatorInformations = this.checkIfElevatorIsBetter(1, elevator, bestScore, referenceGap, bestElevator, floor)
+        //     } else if (floor > elevator.currentFloor && elevator.direction == "up" && direction == elevator.direction) {
+        //         bestElevatorInformations = this.checkIfElevatorIsBetter(2, elevator, bestScore, referenceGap, bestElevator, floor)  
+        //     } else if (floor < elevator.currentFloor && elevator.direction == "down" && direction == elevator.direction) {
+        //         bestElevatorInformations = this.checkIfElevatorIsBetter(2, elevator, bestScore, referenceGap, bestElevator, floor)
+        //     } else if (elevator.status == "idle") {
+        //         bestElevatorInformations = this.checkIfElevatorIsBetter(3, elevator, bestScore, referenceGap, bestElevator, floor)
+        //     } else {
+        //         bestElevatorInformations = this.checkIfElevatorIsBetter(4, elevator, bestScore, referenceGap, bestElevator, floor)
+        //     }
+        console.log(bestElevatorInformations[1],'scoreretured')
+                
+            bestElevator = bestElevatorInformations[0]
+            bestScore = bestElevatorInformations[1]
+            referenceGap = bestElevatorInformations[2]
+            console.log(referenceGap,'GAPP')   
+            return bestElevator
+        
+        }
+        console.log(referenceGap,'GAPP')
+    })
+}
+        
+   
 
+
+
+    checkIfElevatorIsBetter(scoreToCheck, newElevator, bestScore, referenceGap, bestElevator, floor) {
+        // console.log("checl",referenceGap)
+        if (scoreToCheck < bestScore) {
+            bestScore = scoreToCheck
+            bestElevator = newElevator 
+            referenceGap = Math.abs(newElevator.currentFloor - floor)
+        } else if (scoreToCheck == bestScore) {
+            var gap = Math.abs(newElevator.currentFloor - floor) 
+        }       if (referenceGap > gap) {
+            bestElevator = newElevator
+            referenceGap = gap
+        }
+        // console.log("allo")
+        // console.log(bestElevator)
+        // console.log(bestElevator,"bestelevator")
+        // bestElevatorInformations = [bestElevator, bestScore, referenceGap]
+        console.log(bestScore,'GAPP')
+        return [bestElevator, bestScore, referenceGap]
+        
+        
+
+        
     }
+
 
 
 
@@ -89,9 +137,9 @@ class Column {
 class Elevator {
     constructor(id, amountOfFloors) {
         this.ID = id
-        this.status = "idle"
-        this.direction = null
-        this.currentFloor = 2
+        this.status = "stopped"
+        this.direction = "up"
+        this.currentFloor = 5
         this.door = new Door(id)
         this.floorRequestButtonList = []
         this.floorRequestList = []
@@ -117,53 +165,53 @@ class Elevator {
     }
     
     
-    move() {
+    // move() {
         
-        while (this.floorRequestList.length != 0 ) {
-            console.log("requeslist", this.floorRequestList)
-            let destination = this.floorRequestList[0]
-            this.status = "moving"
-            console.log("current", this.currentFloor)
-            console.log("desti", destination)
-            if (this.currentFloor < destination) {
+    //     while (this.floorRequestList.length != 0 ) {
+    //         console.log("requeslist", this.floorRequestList)
+    //         let destination = this.floorRequestList[0]
+    //         this.status = "moving"
+    //         console.log("current", this.currentFloor)
+    //         console.log("desti", destination)
+    //         if (this.currentFloor < destination) {
                 
-                this.direction = "up"
-                console.log(this.direction)
-                this.sortFloorList()
-                destination = this.floorRequestList[0]
-                while(this.currentFloor < destination) {
-                    this.currentFloor ++
-                    console.log(this.currentFloor)
-                }} else if (this.currentFloor > destination) {
-                    this.direction = "down"
-                    this.sortFloorList()
-                    destination = this.floorRequestList[0]
-                    while(this.currentFloor > destination) {
-                        this.currentFloor --
-                    }
+    //             this.direction = "up"
+    //             console.log(this.direction)
+    //             this.sortFloorList()
+    //             destination = this.floorRequestList[0]
+    //             while(this.currentFloor < destination) {
+    //                 this.currentFloor ++
+    //                 console.log(this.currentFloor)
+    //             }} else if (this.currentFloor > destination) {
+    //                 this.direction = "down"
+    //                 this.sortFloorList()
+    //                 destination = this.floorRequestList[0]
+    //                 while(this.currentFloor > destination) {
+    //                     this.currentFloor --
+    //                 }
                    
-                }
-                this.status = "stopped"
-                this.floorRequestList.shift()
-                console.log(this.floorRequestList)
+    //             }
+    //             this.status = "stopped"
+    //             this.floorRequestList.shift()
+    //             console.log(this.floorRequestList)
 
-            }
+    //         }
             
-        }
+    //     }
           
 
    
 
-    sortFloorList(){
-        if (this.direction == "up") {
-        this.floorRequestList.sort()
-        console.log("sortFloorList", this.floorRequestList)
-        } else if (this.direction == "down"){
-            this.floorRequestList.reverse()
-        }
+    // sortFloorList(){
+    //     if (this.direction == "up") {
+    //     this.floorRequestList.sort()
+    //     console.log("sortFloorList", this.floorRequestList)
+    //     } else if (this.direction == "down"){
+    //         this.floorRequestList.reverse()
+    //     }
 
 
-    }
+    // }
 
 
 
@@ -207,7 +255,8 @@ let myColumn = new Column(1, 10, 2)
 
 
 // console.log(myColumn.elevatorList)
-
+// myColumn.elevatorList[1].currentFloor = 8
+// console.log(myColumn.elevatorList[1])
 myColumn.requestElevator (5, "up")
 
 
